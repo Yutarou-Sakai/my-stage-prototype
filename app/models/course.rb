d9435e6df23bce4b687a9e87c4c230213beba23a
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-21 22:01:08 +0900

    scaffoldsでコースのRESTfulを作成

---
+++ app/models/course.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class Course < ApplicationRecord
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :courses
   root 'static_pages#index'
 end
