0f0d1de5f1ea515e1223e9d54702c461a8a44984
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-21 22:47:32 +0900

    コースのバリデーションを追加

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,4 +9,10 @@
 #  updated_at  :datetime         not null
 #
 class Course < ApplicationRecord
+  validate :title, presence: true
+  validate :description, presence: true, length: { :minimum => 5 }
+
+  def to_s
+    title
+  end
 end
