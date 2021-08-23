25d90805b02c68fe9a83d57c657fb46933a62a45
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-23 16:50:57 +0900

    seedsにcourse_urlを追加してダミーデータを再生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 User.create!(email: 'admin@adimn.com', password: '123456', password_confirmation: '123456')
 
-30.times do
+10.times do
   Course.create!([{
     title: Faker::Educator.course_name,
     description: Faker::TvShows::GameOfThrones.quote, 
+    course_url: Faker::Name.unique.name,
     user_id: User.first.id
   }])
 end
