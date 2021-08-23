8e22e2c5b8865a17b87f354f99a3d6bea8f8ad92
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-23 16:32:14 +0900

    course_urlをユニークになるようにバリデーションを追加

--- app/models/course.rb
+++ app/models/course.rb
@@ -27,6 +27,7 @@
 class Course < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true, length: { :minimum => 5 }
+  validates :course_url, uniqueness: true
 
   has_rich_text :description
 
