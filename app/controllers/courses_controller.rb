7f8b593cf7f73b91b6e83f099abdf1bf8ecba429
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-24 14:11:24 +0900

    seed生成時にpublic_activityのエラーが出ないように修正

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,26 +2,21 @@ class CoursesController < ApplicationController
   before_action :set_course, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
-  # GET /courses or /courses.json
   def index
     @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)
     @courses = @ransack_courses.result.includes(:user)
   end
 
-  # GET /courses/1 or /courses/1.json
   def show
   end
 
-  # GET /courses/new
   def new
     @course = Course.new
   end
 
-  # GET /courses/1/edit
   def edit
   end
 
-  # POST /courses or /courses.json
   def create
     @course = Course.new(course_params)
     @course.user = current_user
@@ -37,7 +32,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /courses/1 or /courses/1.json
   def update
     respond_to do |format|
       if @course.update(course_params)
@@ -50,7 +44,6 @@ class CoursesController < ApplicationController
     end
   end
 
-  # DELETE /courses/1 or /courses/1.json
   def destroy
     @course.destroy
     respond_to do |format|
@@ -60,12 +53,10 @@ class CoursesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.friendly.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def course_params
       params.require(:course).permit(
         :title,
