300b7ee41536c1d4b0f0c8417708fbe09553735f
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-27 22:11:48 +0900

    ユーザー一覧にページネーションを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,9 @@ class UsersController < ApplicationController
   before_action :has_admin?, only: [:index, :show, :edit, :update]
 
   def index
-    @q = User.ransack(params[:q])
-    @users = @q.result(distinct: true)
+    @ransack_users = User.ransack(params[:q])
+    # @users = @ransack_users.result(distinct: true)
+    @pagy, @users = pagy(@ransack_users.result(distinct: true).order(updated_at: :DESC))
   end
 
   def show
