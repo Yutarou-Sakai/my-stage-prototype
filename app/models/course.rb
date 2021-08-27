babc5f2c39eb94d91b9fa93c9bd24f70829c73a0
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-27 15:28:38 +0900

    権限のないユーザーに編集・削除ボタンが表示されないように修正

--- app/models/course.rb
+++ app/models/course.rb
@@ -58,6 +58,6 @@ class Course < ApplicationRecord
   end
 
   def bought_course(user)
-    self.enrollments.where(user_id: [user_id], course_id: [self.id].empty?)
+    self.enrollments.where(user_id: [user_id], course_id: [self.id]).empty?
   end
 end
